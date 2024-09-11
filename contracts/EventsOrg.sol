7cf74ed1f0306c31ab1e1c62e9377a80bc210c76
Gifftybabe <ulokangozi@gmail.com>
2024-09-11 06:05:24 +0100

    Creating an event

--- contracts/EventsOrg.sol
+++ contracts/EventsOrg.sol
@@ -16,4 +16,14 @@ contract EventsOrg {
     mapping (address => mapping (uint => uint)) public tickets; // maps an address (attendee) to another mapping that associates a uint event ID with a uint ticket count.
 
     uint public nextId; // keep track of the next available unique ID for creating new events
+
+
+    function createEvent(string calldata _eventName, uint _date, uint _ticketPrice, uint _ticketCount) external {
+        require(block.timestamp < _date, 'You cannot create an event for past date');
+        require(_ticketCount > 0, 'Ticket count must be greater than zero');
+        // the organizer is the msg.sender who is the person calling the function.
+        // During event creation time, the number of remaining ticket is the same as the ticketCount since no one has gotten the ticket yet reason why we passed the ticketCount twice
+        events[nextId] = Event(msg.sender, _eventName, _date, _ticketPrice, _ticketCount, _ticketCount); 
+        nextId++;
+    }
 }
